e4231850fdcda1a4d8f6a95d75b84f42db89315c
Artem <artempavlov02@mail.ru>
2021-07-24 18:22:08 +0300

    Урок 46. Добавление скрипта. Контекст вызова функций, как работает this внутри объекта, внутри обработчика событий со стрелочной и обычной

--- 03/03.46/js/script.js
+++ 03/03.46/js/script.js
@@ -1,25 +1,98 @@
 'use strict';
 
-function User(name, id) {
-	this.name = name;
-	this.id = id;
-	this.human = true;
-	this.helllo = function () {
-		console.log(`Hello, ${this.name}`);
-	};
-}
+// function showThis() {
+// 	console.log(this);
+// }
 
-User.prototype.exit = function (name) {
-	console.log(`Пользователь ${this.name} ушёл`);
-}
+// showThis();
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+// 1)this = window, если есть use strict = underfind
 
-ivan.helllo();
-alex.helllo();
 
-ivan.exit();
+// function showThis1(a, b) {
+// 	// console.log(this);
+// 	function sum() {
+// 		// console.log(this);
+// 		return a + b;
+// 	}
+// 	console.log(sum());
+// }
 
-console.log(ivan);
-console.log(alex);
+// showThis1(4, 5);
+
+
+// const obj = {
+// 	a: 20,
+// 	b: 15,
+// 	sum: function () {
+// 		function shout() {
+// 			console.log(this);
+// 		}
+// 		shout();
+// 	}
+// };
+// obj.sum();
+
+// 2)Контекст(this) у методов объекта - сам объект
+
+// function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// 	this.helllo = function () {
+// 		console.log(`Hello, ${this.name}`);
+// 	};
+// }
+
+// let ivan = new User('Ivan', 28);
+
+// 3)this в конструкторах и классах - это новый экземпляр
+
+// function sayName(surname) {
+// 	console.log(this.name + surname);
+// 	console.log(this);
+// }
+
+// const Userr = {
+// 	name: 'John',
+// };
+
+// sayName.call(Userr, 'SSSSSSS');
+// sayName.apply(Userr, ['SSSSSS']);
+
+// function count(num) {
+// 	return this * num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(233));
+
+// 4) Ручаня привязка this: call, apply, bind
+
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', function () {
+// 	this.style.backgroundColor = 'green';
+// });
+
+// const obj = {
+// 	num: 5,
+// 	sayNum: function () {
+// 		const say = () => {
+// 			console.log(this.num);
+// 		};
+// 		say();
+// 	}
+// };
+
+// obj.sayNum();
+
+// const double = a => a * 2;
+// console.log(double(4));
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', (e) => {
+	const target = e.target;
+	target.style.backgroundColor = 'green';
+});
+
