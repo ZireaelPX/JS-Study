d41c9fe4ca3a158a283a261049f4876452b5edfa
Artem <artempavlov02@mail.ru>
2021-07-25 12:49:52 +0300

    Урок 47. Наследование классов, использование extends, метод super - получает свойства родителя(всегда должен стоять на 1 месте)

--- 03/03.47/js/script.js
+++ 03/03.47/js/script.js
@@ -9,10 +9,23 @@ class Rectangle {
 		return this.height * this.width;
 	}
 }
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(100, 5);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+class ColoredRectangleWithText extends Rectangle {
+	constructor(height, width, text, color) {
+		super(height, width); // вызывает свойства, как у родителя (всегда должна стоять на 1 месте в конструктуре)
+		this.text = text;
+		this.color = color;
+	}
+	showMyProps() {
+		console.log(`Текст: ${this.text}, цвет: ${this.color}`);
+	}
+}
+const div = new ColoredRectangleWithText(25, 20, 'hello', 'green')
 
-
-const square = new Rectangle(10, 10);
-const long = new Rectangle(100, 5);
-console.log(square.calcArea());
-console.log(long.calcArea());
+div.showMyProps();
+console.log(div.calcArea());
+//
 
