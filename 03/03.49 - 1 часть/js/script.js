5be4839c81ab18acc2b8ff4763b13052bf70cc65
Artem <artempavlov02@mail.ru>
2021-07-25 21:39:08 +0300

    разделение уроков 49 на 1 и 2 часть

--- 03/03.49 - 1 часть/js/script.js
+++ 03/03.49 - 1 часть/js/script.js
@@ -6,3 +6,10 @@ const log = function (a, b, ...rest) {
 	console.log(a, b, rest);
 }
 log('basic', 'rest', 'operator', 'usage');
+
+
+
+function calcOrDouble(bumber, basis = 2) {
+	console.log(bumber * basis);
+}
+calcOrDouble(3);
