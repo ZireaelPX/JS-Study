3abebccf62745a919af795f85c66c647838a8580
Artem <artempavlov02@mail.ru>
2021-07-27 22:23:07 +0300

    Весь 2 раздел записан в тетрадь. след: день 28 - планирую записать раздел 3

--- #повторение/#Проверка скриптов/js/script.js
+++ #повторение/#Проверка скриптов/js/script.js
@@ -2,3 +2,23 @@ console.log(null + true);
 console.log(null + false);
 console.log(false - null);
 console.log(true - null);
+console.log(1 && '');
+console.log(typeof (1 && ''));
+console.log(1 && 0);
+console.log(1 && null);
+console.log(1 || null);
+
+if (1 && true && 0) {
+	console.log(1);
+} else {
+	console.log(2);
+}
+
+console.log(3 && 2);
+
+
+const sadBTN = document.querySelector('button');
+
+sadBTN.addEventListener('click', (e) => {
+	e.target.remove();
+});
