9cba60a11ed1309387724ab4459877f6abf4a194
Artem <artempavlov02@mail.ru>
2021-08-08 13:52:48 +0300

    Повторил темы: логические операторы, операторы сравнения, оператор объединения

--- #повторение и codewars/#Проверка скриптов/js/script.js
+++ #повторение и codewars/#Проверка скриптов/js/script.js
@@ -24,3 +24,78 @@ sadBTN.addEventListener('click', (e) => {
 });
 console.log('a' * 'b');
 
+
+let a = 5;
+console.log('Я' > 'А');
+
+console.log(undefined === 0);
+console.log(5 > 4);//true
+console.log("ананас" > "яблоко");//false
+console.log("2" > "12");//true
+console.log("846" > "524");//true
+console.log(undefined == null);//true
+console.log(undefined === null);//false
+console.log(null == "\n0\n");//false
+console.log(null === +"\n0\n");//false
+
+
+let firstName = null;
+let lastName = null;
+let nickName = "Суперкодер";
+
+
+// alert(firstName || lastName || nickName || "Аноним");
+
+console.log(1 && 2);
+// console.log(alert(1) || 2 || alert(3));
+console.log(alert(1));
+
+
+
+function age(age) {
+	if (age >= 14 && age <= 90) {
+		console.log('Находится в заданном диапозоне');
+	} else {
+		console.log('Ошибка');
+	}
+}
+age(1);
+
+function age(age) {
+	if (!(age >= 14 && age <= 90)) {
+		console.log('Не находится в данном диапозоне');
+	} else {
+		console.log('Находится в заданном диапозоне');
+	}
+}
+age(15);
+
+
+function age(age) {
+	if (age <= 14 || age >= 90) {
+		console.log('Verno, ' + age + 'не находится в диапозоне от 14 до 90');
+	} else {
+		console.log('Neverno, ' + age + ' находится в диапозоне от 14 до 90');
+	}
+}
+age(15);
+
+console.log(1 && null);
+console.log(1 && 2 || null);
+
+console.log(null || 0);
+
+if (-1 || 0) {
+	console.log('sad');
+}
+if (-1 && 0) {
+	console.log('sad');
+} else {
+	console.log('nosad');
+}
+if (null || -1 && 1) {
+	console.log('sad');
+} else {
+	console.log('nosad');
+}
+
