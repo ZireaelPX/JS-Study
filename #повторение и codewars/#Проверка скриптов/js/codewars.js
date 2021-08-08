50745e3fc1e4766e7a3a857465a77e6c86c22c85
Artem <artempavlov02@mail.ru>
2021-08-08 17:08:03 +0300

    Codewars. Задача на break и continue в цикле for

--- #повторение и codewars/#Проверка скриптов/js/codewars.js
+++ #повторение и codewars/#Проверка скриптов/js/codewars.js
@@ -156,3 +156,60 @@ function pickIt(arr) {
 	return [odd, even];
 }
 pickIt([10, 20, 30]);
+
+//пример 
+function findFirstOddNumber(arr) {
+	var result;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 1) {
+			result = arr[i];
+			break;
+		}
+	}
+	console.log(result);
+	return result;
+}
+
+findFirstOddNumber([2, 3, 4, 5, 6, 7, 8, 9]);
+
+// Coding in function ```grabDoll```. function accept 1 parameter:```dolls```. it's a string array, a list of some dolls.
+// You need traverse ```dolls``` by using ```for``` loop. If element is  "Hello Kitty" or "Barbie doll", 
+// you should push it to a ```bag```(bag is an array, I've defined in the function); if it's other strings, we should use ```continue``` skip it. 
+// When the ```bag``` has three element, ```bag``` is full. You should use ```break``` jump out the loop; 
+// If ```bag``` is not full, you should traverse ```dolls``` until the last element. 
+// Return the ```bag``` after for loop finished.
+// You should use ```for```, ```break``` and ```continue``` in your code. otherwise, your solution may not pass this kata.
+
+// Моё решение
+function grabDoll(dolls) {
+	let bag = [];
+	//coding here
+	for (let i = 0; i <= dolls.length; i++) {
+		if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+			bag.push(dolls[i]);
+			if (bag.length === 3) {
+				break;
+			}
+		}
+		else {
+			continue;
+		}
+	}
+	console.log(bag);
+	return bag;
+}
+grabDoll(["Mickey Mouse", "Barbie doll", "Hello Kitty", "Hello Kitty", "Hello Kitty", "Snow white"]);
+// codewars
+//1
+function grabDoll(dolls) {
+	var bag = [];
+	for (d in dolls) {
+		if (dolls[d] == "Hello Kitty" || dolls[d] == "Barbie doll") bag.push(dolls[d]);
+		if (bag.length < 3) continue;
+		break;
+	}
+	return bag;
+}
+//2
+grabDoll = dolls =>                         // reduce instead for(...) break continue
+	dolls.filter(i => i == 'Hello Kitty' || i == 'Barbie doll').slice(0, 3);
